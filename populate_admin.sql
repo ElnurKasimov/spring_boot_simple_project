fb78bc44f16859e0bdfb6bea9f317cb35ff37233
ElnurKasimov <ekasimov2012@gmail.com>
2022-12-15 23:32:53 +0200

    added in the root directory sql files for populating role and user tables

---
+++ populate_admin.sql
@@ -0,0 +1,6 @@
+﻿insert into "user" (id, last_name, first_name, email, password)
+values('a6192263-d70f-4a15-960f-074975e3063a', 'admin', 'admin',
+	   'admin@admin.com', '$2a$10$sq.p6upRUoqIGvFUMjy5netCffCYMr2t0Troca/w7EZVYiaOOnRpy');
+
+insert into role_user (role_id, user_id) VALUES
+('fcf12a8d-91e9-4bb6-b60b-53d6d97a9f44', 'a6192263-d70f-4a15-960f-074975e3063a');
---
+++ populate_role.sql
@@ -0,0 +1,2 @@
+﻿insert into role (id, name) values('b7308272-2c87-4138-9680-a9fbbc1fed19', 'ROLE_USER');
+insert into role (id, name) values('fcf12a8d-91e9-4bb6-b60b-53d6d97a9f44', 'ROLE_ADMIN');
